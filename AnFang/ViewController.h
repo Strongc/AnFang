3591188f1dcf1cd90555ed26bf50a6b193674f1f
mac <624191228@qq.com>
2015-09-24 13:27:59 +0800

    1.安装pods插件

---
+++ AnFang/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnFang
+//
+//  Created by mac   on 15/9/24.
+//  Copyright (c) 2015年 wellgood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AnFang/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AnFang
+//
+//  Created by mac   on 15/9/24.
+//  Copyright (c) 2015年 wellgood. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
