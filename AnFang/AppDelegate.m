cf2b90ff4cb6b421095d7b89175df4d3761c77d8
mac <624191228@qq.com>
2015-09-29 14:01:05 +0800

    no message

--- AnFang/AppDelegate.m
+++ AnFang/AppDelegate.m
@@ -64,14 +64,14 @@
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     
     
-    //[httpServer stop];
+    [httpServer stop];
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     
-    //[self startServer];
+    [self startServer];
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
